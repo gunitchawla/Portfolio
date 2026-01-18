309e41157a8cf1291ee533afbb957418197eefbf
gunitchawla <chawlagunit123@gmail.com>
2026-01-18 23:03:48 +0530

    Remove introduction paragraph from About component
    Removed introductory paragraph from About section.

--- components/About.tsx
+++ components/About.tsx
@@ -43,9 +43,7 @@ export default function About() {
                 whileInView={{ opacity: 1 }}
                 transition={{ duration: 1.5 }}
             >
-                <p className="sm:text-[18px] text-[14px] text-gray-400 uppercase tracking-wider">
-                    Introduction
-                </p>
+                
             </motion.div>
 
             <motion.p
@@ -54,11 +52,6 @@ export default function About() {
                 transition={{ delay: 0.5, duration: 1 }}
                 className="mt-4 text-gray-300 text-[17px] max-w-3xl leading-[30px]"
             >
-                I'm a skilled software developer with experience in TypeScript and
-                JavaScript, and expertise in frameworks like React, Node.js, and
-                Three.js. I'm a quick learner and collaborate closely with clients to
-                create efficient, scalable, and user-friendly solutions that solve
-                real-world problems. Let's work together to bring your ideas to life!
             </motion.p>
 
             <div className="mt-20 flex flex-wrap gap-10 justify-center">
